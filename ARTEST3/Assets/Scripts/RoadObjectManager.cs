e75fcd299a293d718a5c7284982731b0c5596414
Ninh Do <doquininh@gmail.com>
2017-02-20 15:14:45 +0100

    forgot to add transparent ground and RoadObjectManager script

---
+++ ARTEST3/Assets/Scripts/RoadObjectManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadObjectManager : MonoBehaviour {
+
+	public GenerateObjects.GPSLocation roadObjectLocation;
+
+	public float dampening = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+	// Subscribe to delegate
+		GPSManager.onRoadObjectSpawn += this.updateLocation;
+	}
+
+	void Destroy() {
+		// Unsubscribe to delegate
+		GPSManager.onRoadObjectSpawn -= this.updateLocation;
+	}
+
+	public void updateLocation() {
+		float distance = GenerateObjects.Haversine(GenerateObjects.myLocation, roadObjectLocation);
+		float bearing = GenerateObjects.CalculateBearing(GenerateObjects.myLocation, roadObjectLocation);
+		transform.position = Vector3.Lerp(transform.position, new Vector3(-Mathf.Cos(bearing) * distance, 0, Mathf.Sin(bearing) * distance), dampening);
+	}
+	
+}
