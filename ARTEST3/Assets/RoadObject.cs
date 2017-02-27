c88e0cd9acfdfcf9a77c8302df645915ea20b5a3
Magnus Warvik <mwarvik@gmail.com>
2017-02-27 10:09:47 +0100

    Started working on movable objects

---
+++ ARTEST3/Assets/RoadObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class RoadObject : MonoBehaviour {
+
+    private bool selected = false;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            selected = !selected;
+        }
+        if(selected) {
+            GetComponentInChildren<Renderer>().material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+        }
+        else {
+            GetComponentInChildren<Renderer>().material.shader = Shader.Find("Diffuse");
+        }
+	}
+}
