104587c921f77caa812e6db65a650372185351a6
Ninh Do <doquininh@gmail.com>
2017-04-05 18:19:12 +0200

    Added rotation of camera using the keyboard (for debugging easier

---
+++ Assets/Scripts/KeyboardControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Rotates the camera using directional input (Arrow keys, WASD, Joypad, etc)
+/// Uses only the horizontal axis to avoid weird rotation
+/// </summary>
+public class KeyboardControl : MonoBehaviour {
+	
+	private const float Speed = 100;
+
+	// Update is called once per frame
+	private void Update() {
+		Vector3 inputVector = new Vector3(0, Input.GetAxis("Horizontal"));
+		transform.Rotate(inputVector * Speed * Time.deltaTime);
+	}
+}
